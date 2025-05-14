254f8624f238bb1c3175d3e745c8aa1f3841f390
DESKTOP-FD57EEK\Joshua Matamorros <yosh.matava@gmail.com>
2025-05-13 22:08:03 -0600

    Add baseurl env variable

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const baseUrl = process.env['BASE_URL'] || 'http://localhost:4200';
 
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   use: {
-    baseURL: 'http://localhost:4200',
+    baseURL: baseUrl,
     browserName: 'chromium',
     headless: true,
   },
